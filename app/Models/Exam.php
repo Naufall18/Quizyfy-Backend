87ce1ba6cb34c12aff64b5bf935879b75cf3fb48
Naufall18 <156426281+Naufall18@users.noreply.github.com>
2026-08-08 22:35:28 +0700

    chore: tambah HasFactory ke model Exam

--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Exam extends Model
 {
+    use HasFactory;
     protected $casts = [
         'start_time' => 'datetime',
         'end_time' => 'datetime',
